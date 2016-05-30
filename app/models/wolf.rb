1f2600a831beb28fbd7502095a078fe2ba89dc5b
vcolavin <vcolavin@gmail.com>
2016-05-30 15:32:55 -0700

    locations now list their objects

--- app/models/wolf.rb
+++ app/models/wolf.rb
@@ -1,4 +1,10 @@
 class Wolf < ActiveRecord::Base
   has_one :location_object, as: :object
   has_one :location, through: :location_objects
+
+  def to_builder
+    Jbuilder.new do |wolf|
+      wolf.name name
+    end
+  end
 end
--- app/views/locations/show.json.jbuilder
+++ app/views/locations/show.json.jbuilder
@@ -2,4 +2,7 @@ json.location do
   json.x @location.x
   json.y @location.y
   json.message "hey yo how ya"
+  json.objects @location.objects do |object|
+    json.thingie object.to_builder
+  end
 end
