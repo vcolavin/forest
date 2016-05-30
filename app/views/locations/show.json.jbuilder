c08b9b32aad68e8653e7117fc7094318b367ce65
vcolavin <vcolavin@gmail.com>
2016-05-30 15:17:26 -0700

    now responds with JSON in all cases, but it's ugly

--- app/views/locations/show.json.jbuilder
+++ app/views/locations/show.json.jbuilder
@@ -1,10 +1,5 @@
-# json.location do
-#   json.x @location.x
-#   json.y @location.y
-#   json.message "hey yo how ya"
-#   # json.objects do
-
-#   # end
-# end
-
-json.message "hey it worked!"
+json.location do
+  json.x @location.x
+  json.y @location.y
+  json.message "hey yo how ya"
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get "location", to: 'locations#show'
+  get "locations", to: 'locations#show', defaults: {format: :json}
+  get "location" , to: 'locations#show', defaults: {format: :json}
 
-  root "locations#show"
+  root "locations#show", defaults: {format: :json}
 end
