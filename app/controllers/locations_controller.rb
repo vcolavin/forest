d7c7b9cec41dc55e6c1ce60e89db98d613532623
vcolavin <vcolavin@gmail.com>
2016-05-30 16:03:22 -0700

    clean up the controller

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -4,44 +4,5 @@ class LocationsController < ApplicationController
         x_coordinate: params[:x],
         y_coordinate: params[:y]
       )
-
-  #   return_json = {}
-
-  # # TODO: move this location finding logic out to a callback or something? It's ugly here.
-  #   if params[:x] && params[:y]
-
-  #     location = Location.find_by(
-  #       x_coordinate: params[:x],
-  #       y_coordinate: params[:y]
-  #     )
-
-  #     if location
-  #       # TODO: move all this hash building out to some JBuilder stuff
-
-  #       return_json[:location] = {
-  #         id: location.id,
-  #         xCoordinate: location.x,
-  #         yCoordinate: location.y
-  #       }
-
-  #       objects = location.objects
-
-  #       return_json[:location][:objects] = objects.map do |object|
-  #         {
-  #           type: object.class.to_s.downcase,
-  #           id: object.id
-  #         }
-  #       end
-
-  #     else
-  #       # TODO: ugh
-  #       return_json[:location] = "nil"
-  #     end
-
-  #   else
-  #     return_json[:location] = "nil"
-  #   end
-
-  #   render json: return_json
   end
 end
--- app/views/locations/show.json.jbuilder
+++ app/views/locations/show.json.jbuilder
@@ -2,7 +2,6 @@ json.location do
   json.x @location.x
   json.y @location.y
   json.objects @location.objects do |object|
-    kind = object.class.to_s.downcase
     json.(object, :kind, :name)
   end
 end
