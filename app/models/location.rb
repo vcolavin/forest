821918f159f2e8625c08f5e82bf626539bf77591
vcolavin <vcolavin@gmail.com>
2016-06-01 12:04:07 -0700

    uncomment the supposed association helper methods

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,14 +1,13 @@
 class Location < ActiveRecord::Base
-  has_many :location_objects#, as: :objects
+  has_many :location_objects #, as: :objects
 
-  # TODO: purportedly gives #wolves and #trees methods
-  # has_many :wolves, :through => :location_objects, :source => :object, :source_type => 'Wolf'
-  # has_many :trees, :through => :location_objects, :source => :object, :source_type => 'Tree'
+  # purportedly gives #wolves and #trees methods
+  has_many :wolves, :through => :location_objects, :source => :object, :source_type => 'Wolf'
+  has_many :trees, :through => :location_objects, :source => :object, :source_type => 'Tree'
 
   validates_uniqueness_of :x_coordinate, :scope => :y_coordinate
 
-  # TODO: could this be a scope?
-  # TODO: It should be handled by the associations. That would be ideal. Why isn't it?
+  # FIXME: This should be handled by the associations. That would be ideal. Why isn't it?
   def objects
     self.location_objects.includes(:object).map do |location_object|
       location_object.object
