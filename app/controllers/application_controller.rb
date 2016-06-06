96b03eb4b1610ccc410b51cce4c22fb46369e115
vcolavin <vcolavin@gmail.com>
2016-06-05 23:08:13 -0700

    layout works

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::API
   # This allows JBuilder "views" to be rendered implicitly
   include ActionController::ImplicitRender
+  include ActionView::Layouts
 end
---
+++ app/views/layouts/application.json.jbuilder
@@ -0,0 +1,3 @@
+json.message "this is the general message"
+
+json.data JSON.parse(yield)
