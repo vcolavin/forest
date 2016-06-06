3874ba946e1ed7b90c07a44733a46a48dfbd7a82
vcolavin <vcolavin@gmail.com>
2016-06-05 23:02:21 -0700

    stub out certain methods for tree rendering

--- app/models/tree.rb
+++ app/models/tree.rb
@@ -1,4 +1,13 @@
 class Tree < ActiveRecord::Base
+  include ModelHelpers
+
   has_one :location_object, as: :object
   has_one :location, through: :location_object
+
+  def to_builder
+    Jbuilder.new do |json|
+      json.kind(kind)
+      # json.url(url)
+    end
+  end
 end
