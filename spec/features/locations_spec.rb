b06036a88b6532c46f41e382d73935f944581b5c
vcolavin <vcolavin@gmail.com>
2016-06-06 19:01:32 -0700

    stub out a couple integration tests

---
+++ spec/features/locations_spec.rb
@@ -0,0 +1,24 @@
+# yuhh integration testing
+describe "GET Location" do
+  it 'finds a location properly (happy path)'
+  it "it well formed; i.e. has the layout"
+  it "responds properly to a location that doesn't exist"
+  it "responds to a request without parameters"
+end
+
+# spec/requests/api/v1/messages_spec.rb
+# describe "Messages API" do
+#   it 'sends a list of messages' do
+#     FactoryGirl.create_list(:message, 10)
+
+#     get '/api/v1/messages'
+
+#     json = JSON.parse(response.body)
+
+#     # test for the 200 status-code
+#     expect(response).to be_success
+
+#     # check to make sure the right amount of messages are returned
+#     expect(json['messages'].length).to eq(10)
+#   end
+# end
