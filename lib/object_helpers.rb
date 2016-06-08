593cdeab646182b72a34ef0b92dcde3e15e3af02
vcolavin <vcolavin@gmail.com>
2016-06-07 17:50:01 -0700

    add helpful TODO reminder

--- lib/object_helpers.rb
+++ lib/object_helpers.rb
@@ -4,6 +4,7 @@ module ObjectHelpers
   end
 
   # this method is hopefully overwritten by every model
+  # TODO: Maybe this shouldn't be here, as it suppresses a useful error?
   def to_builder
     Jbuilder.new do |json|
       json.kind(kind)
