b7eee68f5939b0e2559be4cff97c64a249d5e3dd
vcolavin <vcolavin@gmail.com>
2016-06-11 22:14:37 -0700

    add versioning to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  get "locations", to: 'locations#show', defaults: {format: :json}
-  get "location" , to: 'locations#show', defaults: {format: :json}
+
+  namespace :api, defaults: {format: :json} do
+    scope module: :v1 do
+      get "locations", to: 'locations#show'
+      get "location" , to: 'locations#show'
+    end
+  end
 
   root "locations#show", defaults: {format: :json}
 end
