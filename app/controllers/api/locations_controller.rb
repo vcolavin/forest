bd7517c17f56fc1d82e0d75644c80faf940b35b3
vcolavin <vcolavin@gmail.com>
2016-06-11 22:33:35 -0700

    i think that's how the controller ought to look

--- app/controllers/api/locations_controller.rb
+++ app/controllers/api/locations_controller.rb
@@ -1,14 +1,8 @@
-module Api
-  module V1
-    class LocationsController < ApplicationController
-      # TODO: Is there an index? Some overview of the forest?
-      def show
-        # TODO: this is only the happy path. Handle the unhappy, e.g. what if the params aren't there?
-        @location = Location.find_by(
-            x_coordinate: params[:x],
-            y_coordinate: params[:y]
-          )
-      end
-    end
+class Api::V1::LocationsController < ApplicationController
+  def show
+    @location = Location.find_by(
+        x_coordinate: params[:x],
+        y_coordinate: params[:y]
+      )
   end
 end
