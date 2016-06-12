2deeaae7982d8a7f0115ca8578ab227f51c96f20
vcolavin <vcolavin@gmail.com>
2016-06-12 15:48:52 -0700

    trigger 400 error with malformed request / params

--- app/controllers/api/v1/locations_controller.rb
+++ app/controllers/api/v1/locations_controller.rb
@@ -1,8 +1,10 @@
 class Api::V1::LocationsController < ApplicationController
   def show
     @location = Location.find_by(
-        x_coordinate: params[:x],
-        y_coordinate: params[:y]
+        x_coordinate: params.fetch(:x),
+        y_coordinate: params.fetch(:y)
       )
+
+    raise ActiveRecord::RecordNotFound if @location.nil?
   end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,16 @@
 class ApplicationController < ActionController::API
   include ActionController::ImplicitRender
   include ActionView::Layouts
+
+  rescue_from ActiveRecord::RecordNotFound, :with => :record_not_found
+  rescue_from ActionController::ParameterMissing, :with => :poorly_formed_request
+
+  private
+  def record_not_found(error)
+    render json: { error: error.message }, status: 404
+  end
+
+  def poorly_formed_request(error)
+    render json: { error: error.message }, status: 400
+  end
 end
