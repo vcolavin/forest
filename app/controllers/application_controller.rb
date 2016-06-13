329f86787a6fdd736d6b961f13f386da91f04333
vcolavin <vcolavin@gmail.com>
2016-06-12 19:18:51 -0700

    better name for error catching method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,14 +3,14 @@ class ApplicationController < ActionController::API
   include ActionView::Layouts
 
   rescue_from ActiveRecord::RecordNotFound, :with => :record_not_found
-  rescue_from ActionController::ParameterMissing, :with => :poorly_formed_request
+  rescue_from ActionController::ParameterMissing, :with => :paramenter_missing
 
   private
   def record_not_found(error)
     render json: { error: error.message }, status: 404
   end
 
-  def poorly_formed_request(error)
+  def paramenter_missing(error)
     render json: { error: error.message }, status: 400
   end
 end
