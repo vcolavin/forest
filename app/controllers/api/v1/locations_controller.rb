1cea2223b6b2b08605a7afd3e3b9760ef98c0f6d
vcolavin <vcolavin@gmail.com>
2016-06-15 17:09:30 -0700

    remove intentionally breaking code

--- app/controllers/api/v1/locations_controller.rb
+++ app/controllers/api/v1/locations_controller.rb
@@ -1,8 +1,6 @@
 class Api::V1::LocationsController < ApplicationController
   def show
 
-    fail "big"
-
     @location = Location.find_by(
         x_coordinate: params.fetch(:x),
         y_coordinate: params.fetch(:y)
