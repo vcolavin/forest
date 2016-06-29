c02374ff89bebd4edec3c5b9843c3391e71deaf4
vcolavin <vcolavin@gmail.com>
2016-06-28 23:09:51 -0700

    change location numbers to allow movement in all directions

--- spec/features/locations_spec.rb
+++ spec/features/locations_spec.rb
@@ -3,10 +3,10 @@ require 'rails_helper'
 describe "Locations API", type: :request do
   context "is well formed" do
     before :all do
-      @location = Location.create(x_coordinate: 1, y_coordinate: 2)
+      @location = Location.create(x_coordinate: 2, y_coordinate: 2)
       @wolf = Wolf.create(name: "joseph", location: @location)
 
-      get '/api/v1/location?x=1&y=2'
+      get '/api/v1/location?x=2&y=2'
       @json = JSON.parse(response.body)
     end
 
@@ -20,7 +20,7 @@ describe "Locations API", type: :request do
       location = @json['data']['location']
 
       expect(location).not_to be_nil
-      expect(location['x']).to eq(1)
+      expect(location['x']).to eq(2)
       expect(location['y']).to eq(2)
 
       expect(location['actions']).not_to be_nil
@@ -31,6 +31,7 @@ describe "Locations API", type: :request do
 
     it "wolf" do
       wolf = @json['data']['location']['objects'].first
+
       expect(wolf['name']).to eq(@wolf.name)
       expect(wolf['kind']).to eq('wolf')
 
