e208a95c0acb3d273f9269506ed2bc2603419d4e
vcolavin <vcolavin@gmail.com>
2016-06-30 16:56:51 -0700

    Somehow gets url_for(@location) and location_path working
    Small commit, but it's a doozy with interesting and important implications. And metaprogramming.
    
    Essentially, my main goal was to get url_for(@location) working, as well as location_path(@location). Rails doesn't easily let you mess with path helpers, but you can metaprogram your way into them.
    
    This was way less painful than I expected, but now there is some untested and very brittle code in routes.rb. I'm gonna try and test it now.

--- config/routes.rb
+++ config/routes.rb
@@ -8,3 +8,13 @@ Rails.application.routes.draw do
 
   root "api/v1/locations#show", defaults: {format: :json}, x: 1, y: 1
 end
+
+# Are you looking for the broken thing? This might be the source of the breaking.
+Rails.application.routes.named_routes.path_helpers_module.module_eval do
+  def location_path(location = nil, options = {})
+    if location
+      options.merge!(x: location.x, y: location.y)
+    end
+    api_v1_location_path(options)
+  end
+end
