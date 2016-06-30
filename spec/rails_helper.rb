475eb52ca6c292da12063f9f59015268a7dbec1e
vcolavin <vcolavin@gmail.com>
2016-06-30 15:33:50 -0700

    fix issue where running tests wiped the test db

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,4 +1,3 @@
-ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'spec_helper'
@@ -6,6 +5,7 @@ require 'rspec/rails'
 require 'database_cleaner'
 require 'shoulda/matchers'
 ActiveRecord::Migration.maintain_test_schema!
+
 
 RSpec.configure do |config|
   config.before(:suite) do
