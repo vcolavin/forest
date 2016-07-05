0fb8a511cf1d9856c6801eeb30bd97b3b6e6ac1d
vcolavin <vcolavin@gmail.com>
2016-07-05 12:04:26 -0700

    WIP spec for url helpers

---
+++ spec/helpers/helper_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+describe "location url and path helpers", type: :helper do
+  before :all do
+    @location = Location.create(x_coordinate: 1, y_coordinate: 2)
+  end
+
+  # these tests are necessary because I've metaprogrammed over the path helper logic inside of config/routes.rb
+  it "location path helper works" do
+    expect(location_path(@location)).to eq("/api/v1/location?x=1&y=2")
+  end
+
+  it "location url helper works" do
+    expect(url_for(@location)).to eq("/api/v1/location?x=1&y=2")
+  end
+end
