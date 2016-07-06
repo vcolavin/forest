42b416ea4983d7d863c9db35855bcc49e0a7a8f5
vcolavin <vcolavin@gmail.com>
2016-07-06 15:30:25 -0700

    Move action logic from JBuilder to presenter
    Also adds logic in presenter to ensure that impossible actions (e.g. moving off the map) is not presented as an option.

--- app/presenters/location_presenter.rb
+++ app/presenters/location_presenter.rb
@@ -14,21 +14,56 @@ class LocationPresenter
     end
   end
 
-  # TODO: these ought to return nil for locations that don't exist
+  def actions
+    actions = {}
+    possible_actions = ["north","south","east","west"]
+
+    possible_actions.each do |action|
+      action_name = "#{action}_url"
+      url = eval("self.#{action_name}")
+
+      if url
+        actions.merge!({action_name => url})
+      end
+    end
+
+    actions
+  end
+
   def north_url
-    location_path(@location.north)
+    north = @location.north
+    if north
+      location_path(north)
+    else
+      nil
+    end
   end
 
   def south_url
-    location_path(@location.south)
+    south = @location.south
+    if south
+      location_path(south)
+    else
+      nil
+    end
   end
 
   def east_url
-    location_path(@location.east)
+    east = @location.east
+    if east
+      location_path(east)
+    else
+      nil
+    end
   end
 
   def west_url
-    location_path(@location.west)
+    west = @location.west
+    if west
+      location_path(west)
+    else
+      nil
+    end
   end
 
   private
--- app/views/api/v1/locations/show.json.jbuilder
+++ app/views/api/v1/locations/show.json.jbuilder
@@ -2,14 +2,7 @@ json.location do
   json.x(@location_presenter.x)
   json.y(@location_presenter.y)
 
-  json.actions do
-    # TODO: these need to not render if the direction does not exist
-    # probably comes from a presenter#directions method
-    json.north(@location_presenter.north_url)
-    json.south(@location_presenter.south_url)
-    json.east(@location_presenter.east_url)
-    json.west(@location_presenter.west_url)
-  end
+  json.actions(@location_presenter.actions)
 
   json.objects do
     json.array!(@location_presenter.object_attributes)
