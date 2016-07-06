59830a56ed3d5317ab8ed2cdd644ff5ee5c3dfb3
vcolavin <vcolavin@gmail.com>
2016-07-06 15:13:15 -0700

    add config so URIs render correctly

--- config/application.rb
+++ config/application.rb
@@ -7,8 +7,9 @@ Bundler.require(*Rails.groups)
 module Forest
   class Application < Rails::Application
     config.active_record.raise_in_transactional_callbacks = true
-
-    # Custom directories with classes and modules you want to be autoloadable.
     config.autoload_paths += %W(#{config.root}/lib)
+
+    # Otherwise & is turned into "\u0026" in JSON responses
+    config.active_support.escape_html_entities_in_json = false
   end
 end
