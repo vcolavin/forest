ae4a0e78eea1a086e64ee8657ac46697a964f564
vcolavin <vcolavin@gmail.com>
2016-07-07 19:02:37 -0700

    wrap up location_presenter spec

--- spec/presenters/location_presenter_spec.rb
+++ spec/presenters/location_presenter_spec.rb
@@ -17,10 +17,15 @@ describe LocationPresenter do
     Wolf.destroy_all
   end
 
+  it "has coordinate shortcut methods" do
+    expect(@location_presenter.x).to eq(2)
+    expect(@location_presenter.y).to eq(1)
+  end
+
   it "returns directional URLs" do
-    expect(@location_presenter.north_url).to eq(url_for(@north_location))
-    expect(@location_presenter.east_url).to eq(url_for(@east_location))
-    expect(@location_presenter.west_url).to eq(url_for(@west_location))
+    expect(@location_presenter.north_url).to eq("/api/v1/locations?x=2&y=2")
+    expect(@location_presenter.east_url).to eq("/api/v1/locations?x=3&y=1")
+    expect(@location_presenter.west_url).to eq("/api/v1/locations?x=1&y=1")
   end
 
   it "returns nil for an impossible direction" do
@@ -28,9 +33,20 @@ describe LocationPresenter do
   end
 
   it "collects actions into hash" do
+    actions = @location_presenter.actions
 
+    expect(actions).to be_a(Hash)
+
+    expect(actions["north_url"]).not_to be(nil)
+    expect(actions["east_url"]).not_to be(nil)
+    expect(actions["west_url"]).not_to be(nil)
+    expect(actions["south_url"]).to be(nil)
   end
 
-  it "collects object attributes into hash" do
+  it "collects object attributes into array of hashes" do
+    object_attributes = @location_presenter.object_attributes
+
+    expect(object_attributes).to be_an(Array)
+    expect(object_attributes.first["kind"]).to eq("wolf")
   end
 end
