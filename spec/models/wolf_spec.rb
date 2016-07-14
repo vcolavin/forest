fc67edadb74ac9f721376b1fe9b178666616c02f
vcolavin <vcolavin@gmail.com>
2016-07-14 11:00:12 -0700

    add tests for rabbits and movement

--- spec/models/wolf_spec.rb
+++ spec/models/wolf_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 describe Wolf, type: :model do
   before :all do
     @location = Location.create(x_coordinate: 1, y_coordinate: 1)
+    @north_location = Location.create(x_coordinate: 1, y_coordinate: 2)
     @wolf = Wolf.create(name: "Johnny", location: @location)
   end
 
@@ -11,9 +12,19 @@ describe Wolf, type: :model do
     Wolf.destroy_all
   end
 
-  it {should have_one :location}
-
   it "should have a location" do
-    expect(@wolf.location).to be(@location)
+    expect(@wolf.location).to eq(@location)
+  end
+
+  context "movement methods" do
+    it "should be able to move" do
+      @wolf.go_north!
+      expect(@wolf.location).to eq(@north_location)
+    end
+
+    it "should not be able to move off the map" do
+      @wolf.go_west!
+      expect(@wolf.location).to eq(@location)
+    end
   end
 end
