5038afbe1d48cc67b129e1ac2a5767d2e03f3338
vcolavin <vcolavin@gmail.com>
2016-07-21 14:45:28 -0700

    get rid of multi-inherited behavior

--- app/models/rabbit.rb
+++ app/models/rabbit.rb
@@ -1,6 +1,5 @@
 class Rabbit < ActiveRecord::Base
   include ObjectHelpers
-  include Movable
 
   has_one :location_object, as: :object
   has_one :location, through: :location_object, inverse_of: :rabbits
--- app/models/wolf.rb
+++ app/models/wolf.rb
@@ -1,6 +1,5 @@
 class Wolf < ActiveRecord::Base
   include ObjectHelpers
-  include Movable
 
   has_one :location_object, as: :object
   has_one :location, through: :location_object, inverse_of: :wolves
