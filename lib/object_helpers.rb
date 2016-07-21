2e69b45138bd8613dbdc852003e98eff3b631fa4
vcolavin <vcolavin@gmail.com>
2016-07-21 15:56:29 -0700

    clean things up a bit

--- lib/object_helpers.rb
+++ lib/object_helpers.rb
@@ -7,8 +7,6 @@ module ObjectHelpers
     self.class.to_s.downcase
   end
 
-  # this method is hopefully overwritten by every model
-  # TODO: Maybe this shouldn't be here, as it suppresses a useful error?
   def to_builder
     Jbuilder.new do |json|
       json.kind(kind)
